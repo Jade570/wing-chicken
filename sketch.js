1f5b2050a9ede3f8af7127ee2c8a775da859d027
jade570 <chaelyoung2naver.com>
2020-04-26 16:09:19 +0900

    add robotcontrol

--- sketch.js
+++ sketch.js
@@ -36,6 +36,39 @@ let rlcheck = [];
 let clapcheck = [];
 let robots;
 
+function robotcontrol(){
+  if(frameCount % 30 == 0){
+    for(let i = 0; i<robots; i++){
+      if (walktoken[i] == true){
+        walktoken[i] = false;
+      }
+      else{
+        walktoken[i] = true;
+        if(rlcheck[i] == true){
+          rlcheck[i] = false;
+          llcheck[i] = true;
+        }
+        else{
+          rlcheck[i] = true;
+          llcheck[i] = false;
+        }
+      }
+    }
+  }
+
+  if(frameCount % 30 == 15 || frameCount % 30 == 0 ){
+    for(let i = 0; i<robots; i++){
+      if (bendcheck[i] == true){
+        bendcheck[i] = false;
+      }
+      else{
+        bendcheck[i] = true;
+      }
+    }
+  }
+}
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   frameRate(30);
@@ -89,56 +122,24 @@ lights();
 
 translate(0,0,-600);
 rotateX(radians(-90));
-//rotateZ(radians(90));
-// draw a grid on xy plane
+//rotateZ(radians(35));
 
 
-let h1 = Humanoid(xaxis[0], yaxis[0], zaxis[0], xrot[0], yrot[0], zrot[0], lax[0], lay[0], laz[0], lfax[0], lfay[0], lfaz[0], rax[0], ray[0], raz[0], rfax[0], rfay[0], rfaz[0], ltx[0], lty[0], ltz[0], llx[0], lly[0], llz[0], rtx[0], rty[0], rtz[0], rlx[0], rly[0], rlz[0]);
-let h2 = Humanoid(xaxis[1], yaxis[1], zaxis[1], xrot[1], yrot[1], zrot[1], lax[1], lay[1], laz[1], lfax[1], lfay[1], lfaz[1], rax[1], ray[1], raz[1], rfax[1], rfay[1], rfaz[1], ltx[1], lty[1], ltz[1], llx[1], lly[1], llz[1], rtx[1], rty[1], rtz[1], rlx[1], rly[1], rlz[1]);
-let h3 = Humanoid(xaxis[2], yaxis[2], zaxis[2], xrot[2], yrot[2], zrot[2], lax[2], lay[2], laz[2], lfax[2], lfay[2], lfaz[2], rax[2], ray[2], raz[2], rfax[2], rfay[2], rfaz[2], ltx[2], lty[2], ltz[2], llx[2], lly[2], llz[2], rtx[2], rty[2], rtz[2], rlx[2], rly[2], rlz[2]);
+//create robots
+for(let i = 0; i<robots; i++){
+  Humanoid(xaxis[i], yaxis[i], zaxis[i], xrot[i], yrot[i], zrot[i], lax[i], lay[i], laz[i], lfax[i], lfay[i], lfaz[i], rax[i], ray[i], raz[i], rfax[i], rfay[i], rfaz[i], ltx[i], lty[i], ltz[i], llx[i], lly[i], llz[i], rtx[i], rty[i], rtz[i], rlx[i], rly[i], rlz[i]);
+}
 
 
-
+//robot control part
 leftsidewalk(0);
 clappingarms(0);
 
 walk(1);
-walkingarms(1);
+waggingarms(1);
 
 rightsidewalk(2);
-clappingarms(2);
+walkingarms(2);
 
-if(frameCount % 30 == 0){
-  for(let i = 0; i<robots; i++){
-    if (walktoken[i] == true){
-      walktoken[i] = false;
-    }
-    else{
-      walktoken[i] = true;
-      if(rlcheck[i] == true){
-        rlcheck[i] = false;
-        llcheck[i] = true;
-      }
-      else{
-        rlcheck[i] = true;
-        llcheck[i] = false;
-      }
-    }
-  }
-}
-
-if(frameCount % 30 == 15 || frameCount % 30 == 0 ){
-  for(let i = 0; i<robots; i++){
-    if (bendcheck[i] == true){
-      bendcheck[i] = false;
-    }
-    else{
-      bendcheck[i] = true;
-    }
-  }
-}
-
-
-
-
+robotcontrol();
 }
